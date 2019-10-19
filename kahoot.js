c64da3075d12c1a6316cf019d2097aedcbf5b609
Dany <53636771+Dany-LF@users.noreply.github.com>
2019-10-19 13:17:47 +0200

    Update kahoot.js

--- kahoot.js
+++ kahoot.js
@@ -6,13 +6,13 @@ var colors = require("colors");
 
 var p = "YouTube: Dany"
 
-if(process.argv.length <= 2) {
+if(process.argv.length <= 2) {//Si l'argument 2 est vide
 	console.log("Utilisation: node kahoot.js <pin> <nombre de bots>".green);
 	console.log("Exemple: \nnode kahoot.js 123456 120".green);
 	process.exit(-1);
 }
 
-if(300 <= process.argv[3]) {
+if(300 <= process.argv[3]) {//Si l'argument 3 est supérieur ou égale à 300
 	console.log("ATTENTION: Essayer d'envoyer plus de 300 bots renverra une erreur ETIMEDOUT et déconnectera tous les bots!\nCela peut également provoquer un crash de votre connexion Internet en fonction du nombre de sockets qui peuvent être gérés en même temps...".yellow)
 	console.log("Essayer d'exécuter ce script en ligne pour plus de bande passante(sur un vps ou un rdp par exemple).".green)
 }
