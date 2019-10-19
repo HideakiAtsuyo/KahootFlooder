23678d393e5e14326de2bb21255e805cb6cb0028
Dany <53636771+Dany-LF@users.noreply.github.com>
2019-10-19 06:05:33 +0200

    Upload

---
+++ kahoot.js
@@ -0,0 +1,40 @@
+var Kahoot = require("kahoot.js-updated");
+var colors = require("colors");
+
+var p = "YouTube: Dany"
+
+if(process.argv.length <= 2) {//si l'argument est inférieur ou égale à 2
+	console.log("Utilisation: node kahoot.js <pin> <nombre de bots>".green);
+	console.log("Exemple: \nnode kahoot.js 123456 120".green);//Il renvoie les 2 console.log
+	process.exit(-1);
+}
+
+if(300 <= process.argv[3]) {//Si le troisième argument est supérieur ou égale à 300
+	console.log("ATTENTION: Essayer d'envoyer plus de 300 bots renverra une erreur ETIMEDOUT et déconnectera tous les bots!\nCela peut également provoquer un crash de votre connexion Internet en fonction du nombre de sockets qui peuvent être gérés en même temps...".yellow)
+	console.log("Essayer d'exécuter ce script en ligne pour plus de bande passante(sur un vps ou un rdp par exemple).".green)
+}
+
+var clients = []; //Ici seront enregistré les clients
+for (var i = 1; i <= process.argv[3]; ++i) {//Tant que la var i ne sera pas égale au nombre du troisième argument alors il continuera à faire des bots
+	clients[i] = new Kahoot;//Création des bots
+}
+console.log("Envoie des bots avec succès");
+var e = 0;
+for (var n in clients) {
+	e++;
+	console.log(`${p} a rejoint: ` + `${e} bots`)
+	clients[n].setMaxListeners(Number.POSITIVE_INFINITY)
+	clients[n].join(process.argv[2], `${p} (`+ `${e})`);
+	clients[n].on("joined", () => {
+		if (n == process.argv[3] - 70) {
+			console.log("Tout les bots sont présents!".cyan)
+		}
+	});
+}
+
+clients[n].on("questionStart", question => {
+	question.answer(0);
+	process.on('uncaughtException', function (exception) {
+		n = 200;
+	});
+});
